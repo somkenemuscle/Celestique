c0684114244a56fe30625366e19534a1d5cf2312
Somkene Ojukwu <somkeneoj@gmail.com>
2024-11-23 16:54:46 +0100

    Added shipping address to cookies and disabled strictMode

--- app/(root)/verify/page.tsx
+++ app/(root)/verify/page.tsx
@@ -1,4 +1,4 @@
-import VerifyPaymentContent from '@/components/VerifyPaymentContent';
+import VerifyPaymentContent from '@/components/verify/VerifyPaymentContent';
 
 export default function VerifyPaymentPage({ searchParams }: { searchParams: { reference?: string } }) {
   const reference = searchParams?.reference;
