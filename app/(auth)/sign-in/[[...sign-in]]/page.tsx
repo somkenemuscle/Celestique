8e2f82ed76b05402af3218da9488f3b063060a64
Somkene Ojukwu <somkeneoj@gmail.com>
2024-11-27 02:23:02 +0100

    signin form name updated

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,9 @@
-
-import SignInForm from "@/components/forms/SignInForm"
+import LoginForm from "@/components/forms/LoginForm"
 
 const SignInPage = () => {
     return (
         <main className="lg:mt-28 sm:mt-20">
-            <SignInForm />
+            <LoginForm />
         </main>
     )
 }
