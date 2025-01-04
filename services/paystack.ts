82468cbb008cabfb5ba424d3bdb23372082d1f9c
Somkene Ojukwu <somkeneoj@gmail.com>
2025-01-04 02:33:32 +0100

    Working on deployment

--- services/paystack.ts
+++ services/paystack.ts
@@ -1,13 +1,13 @@
 import axiosInstance from '@/lib/axiosInstance';
 
 export const initializePayment = async (amount: number, shippingAddress: {}) => {
-  const response = await axiosInstance.post('http://localhost:4000/api/payments/initialize', { amount, shippingAddress });
+  const response = await axiosInstance.post('/payments/initialize', { amount, shippingAddress });
   return response.data;
 };
 
 
 export const verifyPayment = async (reference: string | string[], totalAmount: number) => {
-  const response = await axiosInstance.post('http://localhost:4000/api/payments/verify', {
+  const response = await axiosInstance.post('/payments/verify', {
     reference, totalAmount
   });
 
